ff922eb89d02c04038857fed596c484de6a6c182
Parshant Sharma <125631984+parshant-1002@users.noreply.github.com>
2023-08-03 21:27:07 +0530

    chore: code refactor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-
 import React from 'react';
 import Header from './Header/Header.js';
 import Content from './content/content.js';
 
-
-
 export default function App() {
- 
-  return (
+   return (
     <div>
      <Header/>
      <Content/>
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Header() {
   return (
     <div>
-    <h1 className='bg-danger font-bold text-white text-center border border-dark h-75  w-100 p-3'>TO DOs LIST</h1>
+      <h1
+        className='bg-danger font-bold text-white text-center border border-dark h-75  w-100 p-3'>
+        TO DOs LIST
+      </h1>
     </div>
   )
 }
